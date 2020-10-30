d68477882bedc01ae73927f1badbfe874dd0b174
ad-ab <ad-ab@users.noreply.github.com>
2020-10-30 17:49:30 +0000

    deploy: a72092181ca974cebdc3b2faf878a476b7f8c4b9

---
+++ _app/entry-6b483d4a.js
@@ -0,0 +1,2 @@
+export{G as goto,H as prefetch,I as prefetchRoutes,J as start}from"./navigation-6a7be904.js";
+//# sourceMappingURL=entry-6b483d4a.js.map
--- _app/entry-aaca9c5e.js
+++
@@ -1,2 +0,0 @@
-export{G as goto,H as prefetch,I as prefetchRoutes,J as start,K as stores}from"./client-230c2e85.js";
-//# sourceMappingURL=entry-aaca9c5e.js.map
