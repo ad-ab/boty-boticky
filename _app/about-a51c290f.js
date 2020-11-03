8a21489f9d1221c6eaa6472caf1940e973b8defe
ad-ab <ad-ab@users.noreply.github.com>
2020-11-03 12:43:46 +0000

    deploy: 0c23a010a4626e3a747a116b4781a5c7d70855e6

--- _app/about-a51c290f.js
+++
@@ -1,2 +0,0 @@
-import{S as a,i as s,s as n,e as o,t as r,a as t,c,b as f,d as i,f as e,g as l,j as u,k as p,n as d}from"./navigation-2f821ac9.js";function h(a){let s,n,h,j,k;return{c(){s=o("h1"),n=r("O nás"),h=t(),j=o("p"),k=r("nějaké info")},l(a){s=c(a,"H1",{});var o=f(s);n=i(o,"O nás"),o.forEach(e),h=l(a),j=c(a,"P",{});var r=f(j);k=i(r,"nějaké info"),r.forEach(e)},m(a,o){u(a,s,o),p(s,n),u(a,h,o),u(a,j,o),p(j,k)},p:d,i:d,o:d,d(a){a&&e(s),a&&e(h),a&&e(j)}}}export default class extends a{constructor(a){super(),s(this,a,null,h,n,{})}}
-//# sourceMappingURL=about-a51c290f.js.map
--- _app/entry-a32b79c5.js
+++
@@ -1,2 +0,0 @@
-export{J as goto,K as prefetch,L as prefetchRoutes,M as start}from"./navigation-2f821ac9.js";
-//# sourceMappingURL=entry-a32b79c5.js.map
