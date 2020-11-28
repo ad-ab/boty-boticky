447e7cc1c0a6549304cadc14518eba651c79d39d
ad-ab <ad-ab@users.noreply.github.com>
2020-11-28 00:14:02 +0000

    deploy: f9a2e2a5b94b1c2dd041ad8be853a7b2c6a65a5d

---
+++ _app/entry-20695399.js
@@ -0,0 +1,2 @@
+export{a4 as goto,a5 as prefetch,a6 as prefetchRoutes,a7 as start}from"./navigation-40967794.js";
+//# sourceMappingURL=entry-20695399.js.map
--- _app/entry-538eac4b.js
+++
@@ -1,2 +0,0 @@
-export{a3 as goto,a4 as prefetch,a5 as prefetchRoutes,a6 as start}from"./navigation-6b41dbed.js";
-//# sourceMappingURL=entry-538eac4b.js.map
