a47970f5bd4ea7c1983be92042b6021f4ae6dd26
paladinka <paladinka@users.noreply.github.com>
2020-12-22 20:28:08 +0000

    deploy: 47e28d9473640555b018c504e3a05adbe134b4ec

---
+++ _app/entry-24858c1c.js
@@ -0,0 +1,2 @@
+export{a5 as start}from"./start-82967561.js";
+//# sourceMappingURL=entry-24858c1c.js.map
--- _app/entry-faf0151a.js
+++
@@ -1,2 +0,0 @@
-export{a5 as start}from"./start-6f8ed455.js";
-//# sourceMappingURL=entry-faf0151a.js.map
