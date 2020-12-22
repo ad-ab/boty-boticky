b601ee0c608037127bbe74e641485637edf6e64c
ad-ab <abonyi@seznam.cz>
2020-12-23 00:21:06 +0100

    Small fix

--- src/routes/reklamace.svelte
+++ src/routes/reklamace.svelte
@@ -1,64 +1,62 @@
 <svelte:head>
   <title>Reklamace | Boty Botičky - Prodej dětských bot</title>
-  <meta name="description" content="Více informací o webu boty-boticky.cz.">
+  <meta name="description" content="Více informací o webu boty-boticky.cz." />
 </svelte:head>
 
 <h1>Reklamace</h1>
 <p>
-ZÁRUČNÍ LHŮTA A REKLAMAČNÍ ŘÁD
-V případě, že zboží, které jste od nás obdrželi, je vadné či poškozené, oznamte zjištěné vady co nejdříve prodávajícímu emailem na adresu info@boty-boticky.cz. Případně telefonicky nebo písemně na adresu uvedenou v kontaktech.
-
-a/Vadné nebo poškozené zboží bude vyměněno po vzájemné dohodě a po zaslání reklamovaného zboží na naši adresu na náklady prodávajícího.
-b/ Zboží musí být dodáno kompletně, tak jak bylo převzato od prodávajícího.
-c/ Prodávající nepřebírá odpovědnost za škody vyplývající z nesprávného nakládání se zbožím, nesprávného ošetření a jakýchkoli zásahů, ani za škody způsobené vnějšími událostmi. Na vady tohoto původu se nevztahuje ani poskytovaná záruka.
-d/ Zboží je z hygienických důvodů k reklamaci přijímáno pouze čisté.
-e/ Na zboží poskytujeme záruční dobu danou zákonem – 24 měsíců.
- f/ Reklamační lhůta začíná běžet ode dne doručení zboží prodávajícímu.
-
-
-Pokud má převzaté zboží nedostatky (např. nemá sjednané nebo oprávněně očekávané vlastnosti, nehodí se k obvyklému nebo sjednanému účelu, není kompletní, neodpovídá jeho množství, míra, hmotnost, nebo jakost neodpovídá jiným zákonným, smluvním nebo i předsmluvním parametrům), jedná se o vady zboží, za které prodávající odpovídá.
-
-Kupující může u prodávajícího uplatnit nejpozději do dvou let od převzetí zboží podle svého požadavku nárok na bezplatné odstranění vady nebo na přiměřenou slevu z ceny; není-li to povaze vady neúměrné (zejména nelze-li vadu odstranit bez zbytečného odkladu), lze uplatnit požadavek na dodání nové věci bez vad nebo nové součásti bez vad, týká-li se vada pouze této součásti.
-
-Není-li oprava nebo výměna zboží možná, na základě odstoupení od smlouvy může kupující požadovat vrácení kupní ceny v plné výši.
-
-Během šesti měsíců od převzetí zboží se předpokládá, že vada zboží existovala již při převzetí zboží.
-
-Prodávající není povinen nároku kupujícího vyhovět, pokud prokáže, že kupující před převzetím o vadě zboží věděl nebo ji sám způsobil.
-
-U použitého zboží prodávající neodpovídá za vady odpovídající míře dosavadního používání nebo opotřebení. U věcí prodávaných za nižší cenu prodávající neodpovídá za vadu, pro kterou byla nižší cena sjednána. Místo práva na výměnu má kupující v těchto případech právo na přiměřenou slevu.
-
-Zákonná práva z vad
-
-Prodávající odpovídá za vady vzniklé po převzetí zboží v 24 měsíční záruční době nebo v době použitelnosti uvedené v reklamě, na obalu zboží nebo v připojeném návodu.
-
-V této lhůtě může kupující uplatnit reklamaci a podle své volby požadovat u vady, která znamená podstatné porušení smlouvy (bez ohledu na to, jde-li o vadu odstranitelnou či neodstranitelnou):
-
-
-odstranění vady dodáním nové věci bez vady nebo dodáním chybějící věci;
-bezplatné odstranění vady opravou;
-přiměřenou slevu z kupní ceny; nebo
-vrácení kupní ceny na základě odstoupení od smlouvy.
-
-Podstatné je takové porušení smlouvy, o němž strana porušující smlouvu již při uzavření smlouvy věděla nebo musela vědět, že by druhá strana smlouvu neuzavřela, pokud by toto porušení předvídala.
-
-U vady, která znamená nepodstatné porušení smlouvy (bez ohledu na to, jde-li o vadu odstranitelnou či neodstranitelnou), má kupující nárok na odstranění vady anebo přiměřenou slevu z kupní ceny.
-
-Vyskytla-li se odstranitelná vada po opravě opakovaně (třetí reklamace pro stejnou závadu nebo čtvrtá pro odlišné závady) nebo má zboží větší počet vad (nejméně tři vady současně), může kupující uplatnit právo na slevu z kupní ceny, výměnu zboží nebo odstoupit od smlouvy.
-
-Prodávající neodpovídá za vady vzniklé v důsledku běžného opotřebení nebo nedodržení návodu k použití.
- 
-
-Reklamační postup je uveden níže ve druhé – vlastní části podmínek prodeje.
-
-
-
-
-POSTUP PŘI REKLAMACI
-
-a/ Informujte nás o reklamaci telefonicky nebo emailem.
-b/ Zboží zašlete jako doporučený balík na naši adresu.
-c/ Do zásilky uveďte důvod reklamace a variabilní symbol Vaší objednávky.
-
-Vaši reklamaci vyřídíme co nejrychleji, nejpozději však do 30 dnů od jejího vzniku. Záruční lhůta a reklamační řád se řídí obecně platnými právními předpisy.
-  <p>
+  ZÁRUČNÍ LHŮTA A REKLAMAČNÍ ŘÁD V případě, že zboží, které jste od nás
+  obdrželi, je vadné či poškozené, oznamte zjištěné vady co nejdříve
+  prodávajícímu emailem na adresu info@boty-boticky.cz. Případně telefonicky
+  nebo písemně na adresu uvedenou v kontaktech. a/Vadné nebo poškozené zboží
+  bude vyměněno po vzájemné dohodě a po zaslání reklamovaného zboží na naši
+  adresu na náklady prodávajícího. b/ Zboží musí být dodáno kompletně, tak jak
+  bylo převzato od prodávajícího. c/ Prodávající nepřebírá odpovědnost za škody
+  vyplývající z nesprávného nakládání se zbožím, nesprávného ošetření a
+  jakýchkoli zásahů, ani za škody způsobené vnějšími událostmi. Na vady tohoto
+  původu se nevztahuje ani poskytovaná záruka. d/ Zboží je z hygienických důvodů
+  k reklamaci přijímáno pouze čisté. e/ Na zboží poskytujeme záruční dobu danou
+  zákonem – 24 měsíců. f/ Reklamační lhůta začíná běžet ode dne doručení zboží
+  prodávajícímu. Pokud má převzaté zboží nedostatky (např. nemá sjednané nebo
+  oprávněně očekávané vlastnosti, nehodí se k obvyklému nebo sjednanému účelu,
+  není kompletní, neodpovídá jeho množství, míra, hmotnost, nebo jakost
+  neodpovídá jiným zákonným, smluvním nebo i předsmluvním parametrům), jedná se
+  o vady zboží, za které prodávající odpovídá. Kupující může u prodávajícího
+  uplatnit nejpozději do dvou let od převzetí zboží podle svého požadavku nárok
+  na bezplatné odstranění vady nebo na přiměřenou slevu z ceny; není-li to
+  povaze vady neúměrné (zejména nelze-li vadu odstranit bez zbytečného odkladu),
+  lze uplatnit požadavek na dodání nové věci bez vad nebo nové součásti bez vad,
+  týká-li se vada pouze této součásti. Není-li oprava nebo výměna zboží možná,
+  na základě odstoupení od smlouvy může kupující požadovat vrácení kupní ceny v
+  plné výši. Během šesti měsíců od převzetí zboží se předpokládá, že vada zboží
+  existovala již při převzetí zboží. Prodávající není povinen nároku kupujícího
+  vyhovět, pokud prokáže, že kupující před převzetím o vadě zboží věděl nebo ji
+  sám způsobil. U použitého zboží prodávající neodpovídá za vady odpovídající
+  míře dosavadního používání nebo opotřebení. U věcí prodávaných za nižší cenu
+  prodávající neodpovídá za vadu, pro kterou byla nižší cena sjednána. Místo
+  práva na výměnu má kupující v těchto případech právo na přiměřenou slevu.
+  Zákonná práva z vad Prodávající odpovídá za vady vzniklé po převzetí zboží v
+  24 měsíční záruční době nebo v době použitelnosti uvedené v reklamě, na obalu
+  zboží nebo v připojeném návodu. V této lhůtě může kupující uplatnit reklamaci
+  a podle své volby požadovat u vady, která znamená podstatné porušení smlouvy
+  (bez ohledu na to, jde-li o vadu odstranitelnou či neodstranitelnou):
+  odstranění vady dodáním nové věci bez vady nebo dodáním chybějící věci;
+  bezplatné odstranění vady opravou; přiměřenou slevu z kupní ceny; nebo vrácení
+  kupní ceny na základě odstoupení od smlouvy. Podstatné je takové porušení
+  smlouvy, o němž strana porušující smlouvu již při uzavření smlouvy věděla nebo
+  musela vědět, že by druhá strana smlouvu neuzavřela, pokud by toto porušení
+  předvídala. U vady, která znamená nepodstatné porušení smlouvy (bez ohledu na
+  to, jde-li o vadu odstranitelnou či neodstranitelnou), má kupující nárok na
+  odstranění vady anebo přiměřenou slevu z kupní ceny. Vyskytla-li se
+  odstranitelná vada po opravě opakovaně (třetí reklamace pro stejnou závadu
+  nebo čtvrtá pro odlišné závady) nebo má zboží větší počet vad (nejméně tři
+  vady současně), může kupující uplatnit právo na slevu z kupní ceny, výměnu
+  zboží nebo odstoupit od smlouvy. Prodávající neodpovídá za vady vzniklé v
+  důsledku běžného opotřebení nebo nedodržení návodu k použití. Reklamační
+  postup je uveden níže ve druhé – vlastní části podmínek prodeje. POSTUP PŘI
+  REKLAMACI a/ Informujte nás o reklamaci telefonicky nebo emailem. b/ Zboží
+  zašlete jako doporučený balík na naši adresu. c/ Do zásilky uveďte důvod
+  reklamace a variabilní symbol Vaší objednávky. Vaši reklamaci vyřídíme co
+  nejrychleji, nejpozději však do 30 dnů od jejího vzniku. Záruční lhůta a
+  reklamační řád se řídí obecně platnými právními předpisy.
+</p>
