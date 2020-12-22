e72f01e7107d30541a6f45ded9279d4fed2e14c9
paladinka <73667292+paladinka@users.noreply.github.com>
2020-12-22 22:53:48 +0100

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,7 +4,9 @@
       <a class="" href="/boty" rel="preload">Boty</a>
       <a class="" href="/kontakt" rel="preload">Kontakt</a>
       <a class="" href="/about" rel="preload">Doprava</a>
-       <a class="" href="/about" rel="preload">Obchodní podmínky</a>
+       <a class="" href="/obchod" rel="preload">Obchodní podmínky</a>
+       <a class="" href="/reklamace" rel="preload">Reklamace a vrácení zboží</a>
+       <a class="" href="/gdpr" rel="preload">GDPR</a>
 
     </nav>
   </div>
