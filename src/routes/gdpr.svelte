bc043bdd7e897d627d886ac7150d3f8427e769b3
ad-ab <abonyi@seznam.cz>
2020-12-23 17:17:14 +0100

    Fix gdpr

--- src/routes/gdpr.svelte
+++ src/routes/gdpr.svelte
@@ -1,101 +1,113 @@
 <svelte:head>
   <title>GDPR | Boty Botičky - Prodej dětských bot</title>
-  <meta name="description" content="Stránka s gdpr webu boty-boticky.cz. Zde najdete kontakty kam se obrátit v případě problémů.">
+  <meta
+    name="description"
+    content="Stránka s gdpr webu boty-boticky.cz. Zde najdete kontakty kam se obrátit v případě problémů." />
 </svelte:head>
 
 <h1>GDPR</h1>
 
-<H2>Podmínky ochrany osobních údajů</H2>
+<h2>Podmínky ochrany osobních údajů</h2>
 
- 
-
- 
-
-<H3>I. Základní ustanovení</H3>
+<h3>I. Základní ustanovení</h3>
 
 <p>
-  Správcem osobních údajů podle čl. 4 bod 7 nařízení Evropského parlamentu a Rady (EU) 2016/679 o ochraně fyzických osob v souvislosti se zpracováním osobních údajů a o volném pohybu těchto údajů (dále jen: „GDPR”) je Marie Eisová IČO 73773832 se sídlem Staré Ouholice 64, 277 52 Nová Ves (dále jen: „správce“).
+  Správcem osobních údajů podle čl. 4 bod 7 nařízení Evropského parlamentu a
+  Rady (EU) 2016/679 o ochraně fyzických osob v souvislosti se zpracováním
+  osobních údajů a o volném pohybu těchto údajů (dále jen: „GDPR”) je Marie
+  Eisová IČO 73773832 se sídlem Staré Ouholice 64, 277 52 Nová Ves (dále jen:
+  „správce“).
+</p>
+<p>Kontaktní údaje správce jsou<br />
+  adresa: Vlčí 93, 440 01 Chlumčany<br /> email: info@boty-boticky.cz<br />
+  telefon: 608 467 477</p>
+<p>
+  Osobními údaji se rozumí veškeré informace o identifikované nebo
+  identifikovatelné fyzické osobě; identifikovatelnou fyzickou osobou je fyzická
+  osoba, kterou lze přímo či nepřímo identifikovat, zejména odkazem na určitý
+  identifikátor, například jméno, identifikační číslo, lokační údaje, síťový
+  identifikátor nebo na jeden či více zvláštních prvků fyzické, fyziologické,
+  genetické, psychické, ekonomické, kulturní nebo společenské identity této
+  fyzické osoby. Správce nejmenoval pověřence pro ochranu osobních údajů.
+</p>
+
+<h3>II. Zdroje a kategorie zpracovávaných osobních údajů</h3>
+<p>
+  Správce zpracovává osobní údaje, které jste mu poskytl/a nebo osobní údaje,
+  které správce získal na základě plnění Vaší objednávky. Správce zpracovává
+  Vaše identifikační a kontaktní údaje a údaje nezbytné pro plněné smlouvy
+</p>
+
+<h3>III. Zákonný důvod a účel zpracování osobních údajů</h3>
+<p>
+  Zákonným důvodem zpracování osobních údajů je: plnění smlouvy mezi Vámi a
+  správcem podle čl. 6 odst. 1 písm. b) GDPR a oprávněný zájem správce na
+  poskytování přímého marketingu (zejména pro zasílání obchodních sdělení a
+  newsletterů) podle čl. 6 odst. 1 písm. f) GDPR
 </p>
 <p>
-Kontaktní údaje správce jsou<br>
-
-adresa: Vlčí 93, 440 01 Chlumčany<br>
-email: info@boty-boticky.cz<br>
-
-telefon: 608 467 477
+  Účelem zpracování osobních údajů je :vyřízení Vaší objednávky a výkon práv a
+  povinností vyplývajících ze smluvního vztahu mezi Vámi a správcem; při
+  objednávce jsou vyžadovány osobní údaje, které jsou nutné pro úspěšné vyřízení
+  objednávky (jméno a adresa, kontakt), poskytnutí osobních údajů je nutným
+  požadavkem pro uzavření a plnění smlouvy, bez poskytnutí osobních údajů není
+  možné smlouvu uzavřít či jí ze strany správce plnit a zasílání obchodních
+  sdělení a činění dalších marketingových aktivit. Ze strany správce nedochází k
+  automatickému individuálnímu rozhodování ve smyslu čl. 22 GDPR. S takovým
+  zpracováním jste poskytl/a svůj výslovný souhlas.
 </p>
+
+<h3>IV. Doba uchovávání údajů</h3>
 <p>
-Osobními údaji se rozumí veškeré informace o identifikované nebo identifikovatelné fyzické osobě; identifikovatelnou fyzickou osobou je fyzická osoba, kterou lze přímo či nepřímo identifikovat, zejména odkazem na určitý identifikátor, například jméno, identifikační číslo, lokační údaje, síťový identifikátor nebo na jeden či více zvláštních prvků fyzické, fyziologické, genetické, psychické, ekonomické, kulturní nebo společenské identity této fyzické osoby.
-
-Správce nejmenoval pověřence pro ochranu osobních údajů.
+  Správce uchovává osobní údaje po dobu nezbytnou k výkonu práv a povinností
+  vyplývajících ze smluvního vztahu mezi Vámi a správcem a uplatňování nároků z
+  těchto smluvních vztahů (po dobu 15 let od ukončení smluvního vztahu). Po
+  uplynutí doby uchovávání osobních údajů správce osobní údaje vymaže.
 </p>
- 
 
-<H3>II. Zdroje a kategorie zpracovávaných osobních údajů</H3>
+<h3>V. Příjemci osobních údajů (subdodavatelé správce)</h3>
 <p>
-Správce zpracovává osobní údaje, které jste mu poskytl/a nebo osobní údaje, které správce získal na základě plnění Vaší objednávky.
-
-Správce zpracovává Vaše identifikační a kontaktní údaje a údaje nezbytné pro plněné smlouvy
+  Příjemci osobních údajů jsou osoby podílející se na dodání zboží / služeb /
+  realizaci plateb na základě smlouvy, zajišťující služby provozování e-shopu a
+  další služby v souvislosti s provozováním e-shopu, zajišťující marketingové
+  služby na základě smlouvy zajišťující účetní služby na základě smlouvy.
+  Správce nemá v úmyslu předat osobní údaje do třetí země (do země mimo EU) nebo
+  mezinárodní organizaci. Příjemci osobních údajů ve třetích zemích jsou
+  poskytovatelé mailingových služeb / cloudových služeb
 </p>
- 
 
-<H3>III. Zákonný důvod a účel zpracování osobních údajů</H3>
-<p>
-Zákonným důvodem zpracování osobních údajů je: plnění smlouvy mezi Vámi a správcem podle čl. 6 odst. 1 písm. b) GDPR a oprávněný zájem správce na poskytování přímého marketingu (zejména pro zasílání obchodních sdělení a newsletterů) podle čl. 6 odst. 1 písm. f) GDPR
-</p>
-<p>
-Účelem zpracování osobních údajů je :vyřízení Vaší objednávky a výkon práv a povinností vyplývajících ze smluvního vztahu mezi Vámi a správcem; při objednávce jsou vyžadovány osobní údaje, které jsou nutné pro úspěšné vyřízení objednávky (jméno a adresa, kontakt), poskytnutí osobních údajů je nutným požadavkem pro uzavření a plnění smlouvy, bez poskytnutí osobních údajů není možné smlouvu uzavřít či jí ze strany správce plnit a zasílání obchodních sdělení a činění dalších marketingových aktivit.
-
-Ze strany správce nedochází k automatickému individuálnímu rozhodování ve smyslu čl. 22 GDPR. S takovým zpracováním jste poskytl/a svůj výslovný souhlas.
-</p>
- 
-
- 
-
-<H3>IV. Doba uchovávání údajů</H3>
-<p>
-Správce uchovává osobní údaje po dobu nezbytnou k výkonu práv a povinností vyplývajících ze smluvního vztahu mezi Vámi a správcem a uplatňování nároků z těchto smluvních vztahů (po dobu 15 let od ukončení smluvního vztahu).
-Po uplynutí doby uchovávání osobních údajů správce osobní údaje vymaže.
-</p>
- 
-
-<H3>V. Příjemci osobních údajů (subdodavatelé správce)</H3>
-<p>
-Příjemci osobních údajů jsou osoby podílející se na dodání zboží / služeb / realizaci plateb na základě smlouvy, zajišťující služby provozování e-shopu a další služby v souvislosti s provozováním e-shopu, zajišťující marketingové služby na základě smlouvy
-
-zajišťující účetní služby na základě smlouvy.
-
-Správce nemá v úmyslu předat osobní údaje do třetí země (do země mimo EU) nebo mezinárodní organizaci. Příjemci osobních údajů ve třetích zemích jsou poskytovatelé mailingových služeb / cloudových služeb
-</p>
- 
-
-<H3>VI. Vaše práva</H3>
+<h3>VI. Vaše práva</h3>
 
 <p>
- Za podmínek stanovených v GDPR máte právo na přístup ke svým osobním údajům dle čl. 15 GDPR, právo opravu osobních údajů dle čl. 16 GDPR, popřípadě omezení zpracování dle čl. 18 GDPR.
-
-právo na výmaz osobních údajů dle čl. 17 GDPR, právo vznést námitku proti zpracování dle čl. 21 GDPR a právo na přenositelnost údajů dle čl. 20 GDPR.
-
-právo odvolat souhlas se zpracováním písemně nebo elektronicky na adresu nebo email správce uvedený v čl. III těchto podmínek.
-
-Dále máte právo podat stížnost u Úřadu pro ochranu osobních údajů v případě, že se domníváte, že bylo porušeno Vaší právo na ochranu osobních údajů.
+  Za podmínek stanovených v GDPR máte právo na přístup ke svým osobním údajům
+  dle čl. 15 GDPR, právo opravu osobních údajů dle čl. 16 GDPR, popřípadě
+  omezení zpracování dle čl. 18 GDPR. právo na výmaz osobních údajů dle čl. 17
+  GDPR, právo vznést námitku proti zpracování dle čl. 21 GDPR a právo na
+  přenositelnost údajů dle čl. 20 GDPR. právo odvolat souhlas se zpracováním
+  písemně nebo elektronicky na adresu nebo email správce uvedený v čl. III
+  těchto podmínek. Dále máte právo podat stížnost u Úřadu pro ochranu osobních
+  údajů v případě, že se domníváte, že bylo porušeno Vaší právo na ochranu
+  osobních údajů.
 </p>
- 
 
-<H3>VII. Podmínky zabezpečení osobních údajů</H3>
+<h3>VII. Podmínky zabezpečení osobních údajů</h3>
 <p>
-Správce prohlašuje, že přijal veškerá vhodná technická a organizační opatření k zabezpečení osobních údajů.
-
-Správce přijal technická opatření k zabezpečení datových úložišť a úložišť osobních údajů v listinné podobě, zejména zaheslování počítačů, zaheslování jednotlivých administrativních a účetních programů, zabezpečené místnosti pro uložení dokumentů.
-
-Správce prohlašuje, že k osobním údajům mají přístup pouze jím pověřené osoby.
+  Správce prohlašuje, že přijal veškerá vhodná technická a organizační opatření
+  k zabezpečení osobních údajů. Správce přijal technická opatření k zabezpečení
+  datových úložišť a úložišť osobních údajů v listinné podobě, zejména
+  zaheslování počítačů, zaheslování jednotlivých administrativních a účetních
+  programů, zabezpečené místnosti pro uložení dokumentů. Správce prohlašuje, že
+  k osobním údajům mají přístup pouze jím pověřené osoby.
 </p>
- 
 
-<H3>VIII. Závěrečná ustanovení</H3>
+<h3>VIII. Závěrečná ustanovení</h3>
 <p>
-Odesláním objednávky z internetového objednávkového formuláře potvrzujete, že jste seznámen/a s podmínkami ochrany osobních údajů a že je v celém rozsahu přijímáte.
-
-S těmito podmínkami souhlasíte zaškrtnutím souhlasu prostřednictvím internetového formuláře. Zaškrtnutím souhlasu potvrzujete, že jste seznámen/a s podmínkami ochrany osobních údajů a že je v celém rozsahu přijímáte
-
-Správce je oprávněn tyto podmínky změnit. Novou verzi podmínek ochrany osobních údajů zveřejní na svých internetových stránkách a zároveň Vám zašle novou verzi těchto podmínek Vaši e-mailovou adresu, kterou jste správci poskytl/a.</p>
+  Odesláním objednávky z internetového objednávkového formuláře potvrzujete, že
+  jste seznámen/a s podmínkami ochrany osobních údajů a že je v celém rozsahu
+  přijímáte. S těmito podmínkami souhlasíte zaškrtnutím souhlasu prostřednictvím
+  internetového formuláře. Zaškrtnutím souhlasu potvrzujete, že jste seznámen/a
+  s podmínkami ochrany osobních údajů a že je v celém rozsahu přijímáte Správce
+  je oprávněn tyto podmínky změnit. Novou verzi podmínek ochrany osobních údajů
+  zveřejní na svých internetových stránkách a zároveň Vám zašle novou verzi
+  těchto podmínek Vaši e-mailovou adresu, kterou jste správci poskytl/a.
+</p>
