3c0af9219983f6f5f0642eab5119cea3ceb3eada
paladinka <73667292+paladinka@users.noreply.github.com>
2020-12-23 23:47:23 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,39 +20,39 @@
   <nav>
       
     <h2>Dívčí obuv</h2> 
-    <nav2>
+    <nav>
        <h3>Dívčí holínky</h3>
       <a href="https://www.boty-boticky.cz/boty?season=podzim,jaro&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka.jpg" alt="holinky">
       <h3>Dívčí bačkory</h3>
       <a href="https://www.boty-boticky.cz/boty?season=ba%C4%8Dkory&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/backory_holka.jpg" alt="holinky">
-     </nav2> 
+     </nav> 
       
-        <nav2>
+        <nav>
         <h3>Dívčí celoroční</h3>
         <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/celorocni_holka.jpg" alt="holinky">
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/tenisky_holka.jpg" alt="holinky">
-  </nav2>    
-<nav2>
+  </nav>    
+<nav>
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/zimni_holka.jpg" alt="holinky">
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/letni_holka.jpg" alt="holinky">
 
-  </nav2>    
+  </nav>    
   </nav>
   <nav>
   <h2>Chlapecká obuv</h2>
-    <nav2>
+    <nav>
           <a href="https://www.boty-boticky.cz/boty?season=podzim,jaro&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka.jpg" alt="holinky">
 
           <a href="https://www.boty-boticky.cz/boty?season=ba%C4%8Dkory&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/backory_holka.jpg" alt="holinky">
-   </nav2> 
-            <nav2>
+   </nav> 
+            <nav>
             <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/celorocni_holka.jpg" alt="holinky">
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/tenisky_holka.jpg" alt="holinky">
-    </nav2> 
-              <nav2>
+    </nav> 
+              <nav>
         <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/zimni_holka.jpg" alt="holinky">
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/letni_holka.jpg" alt="holinky"> 
-    </nav2>  
+    </nav>  
        </nav>
 
        </div>
@@ -97,11 +97,6 @@
   }
   
  nav {
-    justify-self: center;
-    display:flex;
-    flex-direction: column;
-  }
-  nav2 {
     justify-self: center;
     display:flex;
     flex-direction: column;
