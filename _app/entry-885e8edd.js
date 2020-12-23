2b77498a741ead1cf14178c2d680bc882f7624ff
paladinka <paladinka@users.noreply.github.com>
2020-12-23 16:52:44 +0000

    deploy: e8875591a466cd4c3744cdb11d58bbc63a2d6ab2

---
+++ _app/entry-885e8edd.js
@@ -0,0 +1,2 @@
+export{a8 as start}from"./start-f91eea38.js";
+//# sourceMappingURL=entry-885e8edd.js.map
--- _app/entry-aef1272a.js
+++
@@ -1,2 +0,0 @@
-export{a8 as start}from"./start-46d57f34.js";
-//# sourceMappingURL=entry-aef1272a.js.map
