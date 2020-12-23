ebd65566945ff99383cc787215ba2bd968ecd6dc
paladinka <73667292+paladinka@users.noreply.github.com>
2020-12-23 19:26:52 +0100

    Update vraceni.svelte

--- src/routes/vraceni.svelte
+++ src/routes/vraceni.svelte
@@ -6,7 +6,8 @@
 <h1>Vrácení zboží</h1>
 <p>
 Internetový prodej má jednu zásadní nevýhodu a tou je, že si Vaše dítě obuv nemůže vyzkoušet. Proto jsme pro Vás připravili rady pro výběr obuvi, které Vám pomohou s výběrem správné velikosti. Protože se udávaná velikost může u různých výrobců lišit, doporučujeme vždy před zakoupením nových botiček přeměřit velikost chodidla.
-
+</p>
+<p>
 Nejdůležitější při výběru dětské obuvi je správně vybrat její velikost, která zaručuje noze zdravý vývoj a růst. Dětská noha roste, ale obuv nikoliv. Pro všechny rodiče je jistě velmi důležité pravidelně kontrolovat, zda dítě z obuvi nevyrostlo. U předškoláků a mladších školáků jednou za 2 až 3 měsíce, u starších školáků po 4 až 5 měsících. Pro správné a pohodlné měření existuje několik metod, avšak po našich zkušenostech doporučujeme následující:
 Postavte dítě v tenkých ponožkách nebo bosou nohou na papír a tužkou mu obkreslete obě nohy. Tužku držte kolmo k podložce, aby nedošlo ke zkreslení obrysu nohy. Obkreslete obě nohy, protože téměř každý člověk má jednu nohu delší a pro správné určení velikosti dětské obuvi je třeba vycházet z té delší.
 Označte si střed paty a střed nejdelšího prstu a změřte vzdálenost těchto dvou bodů.
@@ -14,87 +15,78 @@ Pro zjištění potřebné velikosti dětské obuvi je ke změřené délce ješ
 Po přičtení nadměrku zjistíte, jaký rozměr má mít vnitřní část obuvi.
 Pokud si i přesto nejste jisti s výběrem správné velikosti, můžete nás kontaktovat a my Vám rádi poradíme.
 Dětskou obuv prodáváme již několik let a za tu dobu jsme získali mnoho zkušeností. Snažíme se Vám výběr dětské obuvi zjednodušit a pomoci Vám vybrat tu správnou velikost. U každého modelu uvádíme velikost od výrobce a vnitřní rozměr, který sami důkladně měříme.
-
+</p>
  
-JE MOŽNÉ VÝMĚNIT OBUV ZA JINOU VELIKOST ??
-
+<h3>JE MOŽNÉ VÝMĚNIT OBUV ZA JINOU VELIKOST</h3>
+<p>
 Snažíme se svým zákazníkům vždy vycházet vstříc, a pokud se stane, že vybraná velikost obuvi nevyhovuje, můžete ji vyměnit za velikost jinou. Je nutné udělat si novou objednávku a vybrat požadovanou velikost a obuv kterou vracíte, pak pošlete doporučeně na naši adresu a připište číslo účtu, na který Vám vrátíme peníze. Kus za kus neměníme- nesedí nám poté faktury a nemáme to jak doložit do účetnictví a také je velice pravděpodobné, že než se k nám balík dostane, tak již požadovaná velikost k výměně nemusí být skladem, děkujeme za pochopení.
 
-Formulář ke vrácení najdete zde: Vrácení zboží(2).pdf
+Formulář ke vrácení najdete zde přikládáme do každého balíku.
 
 Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.
-
+</p>
 
 Ing. Helena Tylová, Vlčí 93, 440 01 Chlumčany
-
-Jedná-li se o slovenského zákazníka zboží posílejte jen přes Zásilkovnu a touto cestou můžou vracet zboží samozřejmě i zákazníci z ČR - postup, jak na to je zde níže:
-Vrátit zboží můžete také přes Zásilkovnu. Stačí vyplnit kdo/co vrací (níže je k vytištění formulář k vrácení, ten Vám ve zmenšené podobě dorazí také v balíčku) a nezapoměňte na číslo účtu, kam náslevně vrátíme platbu. Snadná a levná cesta, jak zboží poslat zpět k nám je prostřednictvím Zásilkovny- za tuto službu Vám následně odečteme 2 Euro z vráceného zboží, obsluze stačí nadiktovat kód 97853076 a oni vše vyřídí za Vás. Seznam poboček, kde můžete balík odeslat najdete na www.zasilkovna.cz/pobocky. Zásilkovnu můžete využít, i pokud k Vám balík putoval jiným přepravcem.
-
-I zde je nutné do balíčku přidat info k vrácení:
-
-Formulář ke vrácení najdete zde: Vrácení zboží(2).pdf
-
-Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.
-
+<p>
 Prosíme o strpení- balíčky na pobočce Zásilkovny vyzvedáváme 1x týdně (většinou ve čtvrtek) a následně je postupně rozbalujeme a vracíme platby. Ze zákona máme na vrácení platby 14 dní od převzetí balíčku, buďte tedy prosím, trpěliví, moc děkujeme:-)
-
-KDY MŮŽU OČEKÁVAT ZÁSILKU ??
-
+</p>
+<h3>KDY MŮŽU OČEKÁVAT ZÁSILKU </h3>
+<p>
 Veškeré objednávky se snažíme vyřídit v co nejkratší možné době. To znamená, že je posíláme nejpozději do druhého pracovního dne od přijetí platby na náš účet nebo u platby na dobírku do druhého dne po přijetí objednávky. Všechny zásilky odesíláme jako obchodní balík, u kterého je Českou poštou garantovaná dodací lhůta do druhého pracovního dne od data podání. Pokud jste při registraci vyplnili své tel. číslo, pak Vám před dodáním přijde informativní SMS o čase doručení. Stane-li se Vám, že obchodní balík nedorazí do 3 pracovních dnů, apelujte nejprve na svou pobočku České pošty, protože se někdy stává, že nedostanete do schránky informaci o doručení. Jestli i přesto zásilku neobdržíte, napište nám a my Vám pošleme číslo balíku a informaci, kde se balík nachází.
- 
-JAK MŮŽU VYŘÍDIT REKLAMACI ??
-Na veškeré námi nabízené zboží se vztahuje záruka 24 měsíců.
+ </p>
+<h3>JAK MŮŽU VYŘÍDIT REKLAMACI </h3>
+<p>Na veškeré námi nabízené zboží se vztahuje záruka 24 měsíců.
 
- 
-
-Postup při reklamaci :
+  </p>
+<p>
+Postup při reklamaci :<br>
  
 
 Zboží v očištěném stavu ( nikdy boty neperte! avšak je nutné je očistit- zboží od bláta, s nánosy písku a špíny bude posláno zpět k zákazníkovi k očištění- díky za pochopení) zašlete jako doporučený balík na naši adresu.
 Do zásilky uveďte důvod reklamace a variabilní symbol své objednávky a přiložte doklad o koupi, který je nedílnou součástí každé objednávky a slouží jako záruční list. Případně také číslo svého účtu, budete-li si přát vrátit peníze (v případě, že bude reklamace uznána za oprávněnou).
 Formulář k  reklamaci si můžete stáhnout zde: Reklamace pdf(2).pdf
 Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.
-
+</p>
+<p>
  Vaši reklamaci vyřídíme co nejrychleji, nejpozději však do 30 dnů od jejího vzniku - myšleno ode dne převzetí balíčku. Záruční lhůta a reklamační řád se řídí obecně platnými právními předpisy.
-
-Doručovací adresa :
+</p>
+<p>
+Doručovací adresa :<br>
 
 Ing. Helena Tylová, Vlčí 93, 440 01 Chlumčany
 
 
 Reklamaci můžete poslat i přes Zásilkovnu- platí pro zákazníky z ČR i SK. Stačí vyplnit kdo/co vrací a nezapoměňte na číslo účtu. Snadná a levná cesta zboží zpět k nám je prostřednictvím Zásilkovny, obsluze stačí nadiktovat kód 97853076 a oni vše vyřídí za Vás. Seznam poboček, kde můžete balík odeslat najdete na www.zasilkovna.cz/pobocky. Zásilkovnu můžete využít, i pokud k Vám balík putoval jiným přepravcem.
- 
+ </p>
 
 
-MŮŽU ZBOŽÍ VRÁTIT BEZ UDÁNÍ DŮVODU ??
-
+<h3>MŮŽU ZBOŽÍ VRÁTIT BEZ UDÁNÍ DŮVODU</h3>
+<p>
 Kupující má právo podle §53 ods. 7 občanského zákoníku odstoupit od smlouvy do 14 dnů od převzetí zásilky. Na vrácení zboží ve lhůtě 14 dní bez udání důvodu má zákazník právo vždy, pokud kupuje věc přes internet. Vrátit bez udání důvodu lze nové zboží, které bylo jen vyzkoušené. Zboží zasílá odběratel na vlastní náklady doporučeně na adresu dodavatele. Poté jsou mu vráceny peníze za zboží a při vrácení celé objednávky, také za poštovné (doběrečné proplaceno nebude)- vrátíte- li jen část objednávky, pak poštovné nebude uhrazeno. A pokud bude vrácením části objednávky porušena podmínka pro poštovné zdarma, které je jen u objednávky nad 5000 Kč, tak Vám budou náklady na něj odečteny z vráceného zboží. Děkujeme za pochopení.
 
-
+</p>
 Formulář pro odstoupení od smlouvy si můžete stáhnout zde : Vrácení zboží(2).pdf
-
-Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.
-Balík pošlete jako doporučený balík (záleží na Vás, zda kurýrem nebo poštou) na tuto adresu.
+<br>
+Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.<br>
+Balík pošlete jako doporučený balík (záleží na Vás, zda kurýrem nebo poštou) na tuto adresu.<br><br>
  
 
-Doručovací adresa :
-Ing. Helena Tylová, Vlčí 93, 440 01 Chlumčany
+Doručovací adresa :<br>
+Ing. Helena Tylová, Vlčí 93, 440 01 Chlumčany<br><br>
 
 
 
 Vrátit zboží můžete také přes Zásilkovnu- platí pro zákazníky z ČR i SK. Stačí vyplnit kdo/co vrací (níže je k vytištění formulář k vrácení) a nezapoměňte na číslo účtu. Snadná a levná cesta, jak zboží poslat zpět k nám prostřednictvím Zásilkovny- za tuto službu Vám následně odečteme 60 Kč z vráceného zboží, obsluze stačí nadiktovat kód 97853076 a oni vše vyřídí za Vás. Seznam poboček, kde můžete balík odeslat najdete na www.zasilkovna.cz/pobocky. Zásilkovnu můžete využít, i pokud k Vám balík putoval jiným přepravcem.
-
+<br>
 I zde je nutné do balíčku přidat info k vrácení:
 
 
 Formulář ke vrácení najdete zde: Vrácení zboží(2).pdf
 Vkládejte formulář, prosím, vždy do krabice s vrácenou obuví- nelepte ho na balík apod.,díky za pochopení.
+</p>
 
-Prosíme o strpení- balíčky na pobočce Zásilkovny vyzvedáváme 1x týdně (většinou ve čtvrtek) a následně je postupně rozbalujeme a vracíme platby. Ze zákona máme na vrácení platby 14 dní od převzetí balíčku, buďte tedy prosím, trpěliví, moc děkujeme:-)
-
-
-JAK NAKUPOVAT ??
-
+<h3>JAK NAKUPOVAT</h3>
+<p>
 Nakupovat můžete jako registrovaný i neregistrovaný zákazník. Po vložení zboží do košíku a vyplnění potřebných údajů dojde k uložení a odeslání objednávky. Po odeslání objednávky Vám přijde automatický e-mail s potvrzením o přijetí objednávky (jedná se o informativní email).
 Poté vyčkejte na další zprávu ve které Vás budeme informovat o dostupnosti zboží. Všechno zboží máme skladem, ale prodáváme přes více obchodů. Proto s platbou vyčkejte na potvrzení objednávky.Po zpracování vaší objednávky Vám pošleme potvrzení o dostupnosti zboží a žádost o uhrazení částky.Při platbě prosím uveďte variabilní symbol číslo objednávky pro rychlejší identifikaci platby. Další e-mail, který od nás obdržíte, je informace o přijetí platby a odeslání zásilky.
 </p>
