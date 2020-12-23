9008e915e26d44d57962d5e3d84f53298576ea5a
paladinka <73667292+paladinka@users.noreply.github.com>
2020-12-23 17:28:54 +0100

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -5,26 +5,27 @@
 
 <h1>Doprava</h1>
 
-<p>DOPRAVA
-CENÍK DOPRAVY
-Česká republika
+<p>
+CENÍK DOPRAVY<br><br>
 
-Zásilkovna                            platba převodem       69,- Kč 
+Zásilkovna:
+platba převodem       69,- Kč <br>
 
-                                              platba dobírkou        69 + 30 = 99,- Kč 
+                                              platba dobírkou        69 + 30 = 99,- Kč <br><br>
 
-Česká pošta                                    platba převodem       89,- Kč
+Česká pošta:                                    
+  platba převodem       89,- Kč<br>
 
-                                              platba dobírkou        89 + 30 = 119,- Kč 
+                                              platba dobírkou        89 + 30 = 119,- Kč <br><br>
 
-PPL                                       platba převodem      119,- Kč
+PPL:                                       platba převodem      119,- Kč<br>
 
-                                              platba dobírkou        119 + 30 = 149,- Kč 
+                                              platba dobírkou        119 + 30 = 149,- Kč <br><br>
 
  
 
-V hotovosti na prodejně                                         0,- Kč
+V hotovosti na prodejně                                         0,- Kč<br>
 
-Při nákupu nad 1300,- Kč DOPRAVA ZDARMA
+Při nákupu nad 1500,- Kč DOPRAVA ZDARMA<br>
 
-Při nákupu nad 1600,- Kč DOPRAVA I DOBÍRKA ZDARMA</p>
+</p>
