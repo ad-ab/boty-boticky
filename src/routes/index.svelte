e906b9b1283ed6123abd4f94e14d50691d77a81d
paladinka <73667292+paladinka@users.noreply.github.com>
2020-12-24 10:04:33 +0100

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -31,9 +31,9 @@
   <h2>Chlapecká obuv</h2>
           <a href="https://www.boty-boticky.cz/boty?season=podzim,jaro&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky"> 
       <a href="https://www.boty-boticky.cz/boty?season=ba%C4%8Dkory&gender=D%C3%ADv%C4%8D%C3%AD"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky"><br><br>
-        <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky"><br>
-      <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky"><br><br>
+        <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky">
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky">
+      <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky"><br><br>
       <a href="https://www.boty-boticky.cz/boty?season=jaro,podzim"><img src="https://www.vyprodej-dovoz.cz/boty/boty-fotky/holinky_holka2.jpg" alt="holinky">
     </nav>  
 
